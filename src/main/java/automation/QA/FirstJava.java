f3cea12b2181e6d296c991e60d7d72d89fcfd7c3
Iliya Vasilev <kolumbino8577@gmail.com>
2022-10-22 23:34:31 +0300

    some console interaction

--- src/main/java/automation/QA/FirstJava.java
+++ src/main/java/automation/QA/FirstJava.java
@@ -1,5 +1,32 @@
 package automation.QA;
 
+import java.util.Scanner;
+
 public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("Please enter your name");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String firstName = scanner.next();
+
+        System.out.println("Your name is: " + firstName);
+
+        System.out.println("please enter your age: ");
+
+        int userAge = scanner.nextInt();
+
+        System.out.println("Your age is: " + userAge);
+
+        if (userAge >= 18){
+            System.out.println(firstName + " can drink alcohol legally!!!");
+        }
+        if(userAge < 18){
+            System.out.println(firstName + " can't drink alcohol legally");
+        }
+        if (userAge >= 65){
+            System.out.println(firstName + " is retired");
+        }
+    }
 
 }
